b7d9024928fad976fb40a134832587de4d98240b
Thomas E Cowern <thomas.e.cowern@gmail.com>
2020-08-26 12:10:53 -0400

    completed 3 javascript challenges

---
+++ Week2/Wednesday/Challenge3.js
@@ -0,0 +1,7 @@
+for (let i = 100; i > -1; i--) {
+  if (i > 0) {
+    console.log(i +  " bottles of root beer on the wall...");  
+  } else {
+    console.log("Hey! We need more root beer!");  
+  }
+}
