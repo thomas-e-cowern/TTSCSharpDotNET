cd757f796e2fca419c717a5d13815efd1f3662e7
Thomas E Cowern <thomas.e.cowern@gmail.com>
2020-08-27 15:05:03 -0400

    added challenge 2

---
+++ Week2/Thursday/Homework/Challenge2.js
@@ -0,0 +1,37 @@
+let course = {
+	name: 'JavaScript Applications',
+	awesome: true,
+	teachers: ['Assaf', 'Shane'],
+	students: [
+		{
+			name: 'Steve',
+			computer: {
+				OS: 'Linux',
+				type: 'laptop'
+			}
+		},
+		{
+			name: 'Katy',
+			computer: {
+				OS: 'OSX',
+				type: 'macbook'
+			}
+		},
+		{
+			name: 'Chuck',
+			computer: {
+				OS: 'OSX',
+				type: 'macbook'
+			}
+		}
+
+	],
+	preReqs : {
+		skills : ['html', 'css', 'git'],
+		equipment: {
+			laptop: true,
+			OSOptions: ['linux', 'osx']
+		}
+	}
+};
+
