dc8c1399b01f575fba3ba65e4fa947503551a9f6
Thomas E Cowern <thomas.e.cowern@gmail.com>
2020-08-27 09:58:26 -0400

    updated inclassjs.js

--- Week2/Thursday/InClassJS.js
+++ Week2/Thursday/InClassJS.js
@@ -1,6 +1,55 @@
-address = ["Dad", "Mom", "SonA", "SonB"];
+// address = ["Dad", "Mom", "SonA", "SonB"];
 
-console.log(address[0]);
-console.log(address[1]);
-console.log(address[2]);
-console.log(address[3]);
+// for (let x = 0; x < address.length; x++) {
+//   console.log(address[x]);
+// }
+
+// console.log("address length: " + address.length);
+
+// address.push("Dog");
+
+// console.log("address length: " + address.length);
+
+// missingMember = address.pop();
+
+// console.log(missingMember + " has been removed");
+
+// address.unshift("Kitty");
+
+// for (let x = 0; x < address.length; x++) {
+//   console.log("Family member: " + address[x]);
+// }
+
+// missingFamMember = address.shift();
+
+// console.log(missingFamMember + " is missing.");
+
+// for (let x = 0; x < address.length; x++) {
+//   console.log("Family member: " + address[x]);
+// }
+
+let teachers = ['Shane', 'Zack'];
+
+console.log("teacher 0 is " + teachers[0]);
+console.log("current number of teachers: " + teachers.length);
+
+teachers.push("Asaf");
+console.log("Asaf was added to teachers");
+
+console.log("current number of teachers: " + teachers.length);
+console.log("current teachers: " + teachers);
+
+console.log("about to pop a teacher");
+let teacher1 = teachers.pop();
+console.log("current number of teachers: " + teachers.length);
+ 
+console.log("the teacher popped was " + teacher1);
+
+console.log("about to unshift a teacher");
+teachers.unshift(teacher1);
+
+console.log("current teachers: " + teachers);
+
+let teacher = teachers.shift();
+
+console.log("the teacher shifted was " + teacher);
