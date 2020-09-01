af06e10769494848aa5b537161785168419bd15a
Thomas E Cowern <thomas.e.cowern@gmail.com>
2020-09-01 10:54:10 -0400

    updated index and main.js

--- Week3/Tuesday/main.js
+++ Week3/Tuesday/main.js
@@ -1,7 +1,25 @@
+
 function greetOnLoad () {
   var name = prompt("Hi! What's your name?");
-  document.getElementById("greeting");
   greeting.innerHTML= "Hello " + name + ", it's nice to meet you!";
+
+  var randomNumberResult = Math.floor((Math.random() * 10) + 1);
+  randomNumber.innerHTML = "Todays random number is " + randomNumberResult;
 }
 
 greetOnLoad();
+
+var state = 0;
+
+function modifyColor() {
+  if (state == 0) {
+    document.getElementById("example").style.color = "aqua";
+    document.getElementById("example").style.backgroundColor = "gray";
+    state = 1;
+  } else {
+    document.getElementById("example").style.color = "black";
+    document.getElementById("example").style.backgroundColor = "white";
+    state = 0;
+  }
+  
+}
