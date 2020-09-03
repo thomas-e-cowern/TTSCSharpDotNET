43229659331a08f3acf2534031debf23f58b61fc
Thomas E Cowern <thomas.e.cowern@gmail.com>
2020-09-03 04:28:36 -0400

    homework initial commit

---
+++ Week3/Wednesday/Homework/scripts.js
@@ -0,0 +1,32 @@
+const hands = ["rock", "paper", "scissors"];
+
+function getComputerHand () {
+  let x = parseInt(Math.random()*10)%3;
+  return hands[x];
+}
+
+function getPlayerHand () {
+  let hand = prompt("Enter rock, paper or scissors below", "Enter rock, paper or scissors here").toLowerCase();
+  if (hand == "rock" || hand == "paper" || hand == "scissors") {
+    let winner = compare(getComputerHand(), hand);
+    document.getElementById("results").innerHTML = winner[0];
+    document.getElementById("winner").innerHTML = "And the winner is..." + winner[1];
+    winner = "";
+  } else {
+    getPlayerHand();
+  }
+}
+
+function compare (choice1, choice2) {
+  if (choice1 == "rock" && choice2 == "scissors" || choice1 == "paper" && choice2 == "rock" || choice1 == "scissors" && choice2 == "paper") {
+    console.log("Computer: " + choice1 + ", Player: " + choice2)
+    return ["Computer: " + choice1 + ", Player: " + choice2, "Computer wins!"];
+  } else if (choice2 == "rock" && choice1 == "scissors" || choice2 == "paper" && choice1 == "rock" || choice2 == "scissors" && choice1 == "paper") {
+    console.log("Computer: " + choice1 + ", Player: " + choice2)
+    return ["Computer: " + choice1 + ", Player: " + choice2 ,"Player wins!"];
+  } else {
+    console.log("Computer: " + choice1 + ", Player: " + choice2)
+    return ["Computer: " + choice1 + ", Player: " + choice2, "It's a draw!"];
+  }
+}
+
