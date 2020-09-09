c5c73e04f9dc02922bc4e458bc92546b396f5f40
Thomas E Cowern <thomas.e.cowern@gmail.com>
2020-09-09 18:19:31 -0400

    added gtk project

---
+++ Projects/Test/Test/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Gtk;
+
+namespace Test
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Application.Init();
+            Window win = new Window("My first GTK# Application! ");
+            win.Resize(200, 200);
+
+            //Create a label and put some text in it.
+            Label myLabel = new Label();
+            myLabel.Text = "Hello World!!!!";
+
+            //Cretate a button and alter the text.
+            Button myButton = new Button();
+            myButton.Label = "Me Button";
+
+            //Add the label and button to the form
+            //win.Add(myLabel);
+            win.Add(myButton);
+
+            //Make it all visible
+            win.ShowAll();
+            Application.Run();
+        }
+    }
+}
