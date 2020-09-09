9ea7123b3b99ca5747eb60d14ebca3206ecd2cca
Thomas E Cowern <thomas.e.cowern@gmail.com>
2020-09-08 20:52:22 -0400

    started homework

---
+++ Projects/Homework/Homework/HomeClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Homework
+{
+    public class HomeClass
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("This is a line");
+            ReverseString rString = new ReverseString.ReverseMe("bat");
+            TriangleHomework myTriangle = new TriangleHomework.MakeTriangle(6, 6);
+        }
+    }
+}
