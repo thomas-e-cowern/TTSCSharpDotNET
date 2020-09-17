139cf27efa7eb3056f8dc569c4ddc1653aca3216
Thomas E Cowern <thomas.e.cowern@gmail.com>
2020-09-17 13:40:28 -0400

    updated house homework

--- Week5/Thursday/HouseHomework/HouseHomework/Program.cs
+++ Week5/Thursday/HouseHomework/HouseHomework/Program.cs
@@ -6,7 +6,26 @@ namespace HouseHomework
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string name;
+            int houseArea;
+            string doorColor;
+
+        // Get user name
+            Console.Write("Please enter your name... ");
+            name = Console.ReadLine();    
+
+            // Get house dimensions and door color from user
+            Console.Write("What is the area of your house? ");
+            houseArea = Int32.Parse(Console.ReadLine());
+            Console.Write("What is the color of your door? ");
+            doorColor = Console.ReadLine();
+
+            House newHouse = new House(houseArea, doorColor);
+            Person newPerson = new Person(name, newHouse);
+          
+
+            newPerson.ShowData();
+
         }
     }
 }
