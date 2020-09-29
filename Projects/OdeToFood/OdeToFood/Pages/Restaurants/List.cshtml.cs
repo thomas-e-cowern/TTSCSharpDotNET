b56979c11af25fd84f1a9de8d9f8260b5ef1619a
Thomas E Cowern <thomas.e.cowern@gmail.com>
2020-09-29 11:06:19 -0400

    added detail page

---
+++ Projects/OdeToFood/OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using OdeToFood.Core;
+using OdeToFood.Data;
+
+namespace OdeToFood.Pages.Restaurants
+{
+    public class ListModel : PageModel
+    {
+        private readonly IConfiguration config;
+        private readonly IRestaurantData restaurantData;
+
+        public string Message { get; set; }
+        public IEnumerable<Restaurant> Restaurants { get; set; }
+
+        public ListModel(IConfiguration config, Data.IRestaurantData restaurantData)
+        {
+            this.config = config;
+            this.restaurantData = restaurantData;
+        }
+
+        //In List.cshtml.cs add 'searchTerm' as a parameter to the OnGet() method 
+        //and change GetAll() to GetRestaurantsByName() 
+        //and pass in 'searchTerm'
+        public void OnGet(string searchTerm)
+        {
+            Message = config["Message"];
+            Restaurants = restaurantData.GetRestaurantsByName(searchTerm);
+        }
+    }
+}
